7c6921516fbc09eee28ca904530aca375d6ca016
DanielRodriguezCUNOC <pabloalvarado202130534@cunoc.edu.gt>
2025-12-13 03:35:54 -0600

    Creada home-page

--- app-videojuego/src/pages/home-page.component/home-page.component.ts
+++ app-videojuego/src/pages/home-page.component/home-page.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar.component/navbar.component';
-import { FooterComponent } from '../../components/footer.component/footer.component';
 import { PageLayoutComponent } from '../../components/share/page-layout.component/page-layout.component';
 
 @Component({
